d7e47e029f3c05f62d442dd75a7cdd7dbe0e4b65
JAKE MARK GALLARDEZ SALAZAR <146406096+JakeMark011@users.noreply.github.com>
2023-10-03 23:21:31 +0800

    Add files via upload

---
+++ SAMPLE7.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main (){
+     
+     string x;
+     int y;
+     char z;
+     
+     
+     cout << "Enter a string";
+     getline(cin,x);//gwapo si gwapo
+     cout << "Enter a number ";
+     cin>> y;//13
+     cout << "Enter a Letter";
+     cin >> z;//a
+     
+     x[y] = z;
+     cout<<x;
+    
+    
+    
+     return 0 ;
+    
+    
+    
+  } 
+    
+    
+    
+
